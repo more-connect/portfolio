c0b01513fc9e359fafcef2a8042260d29bbe3993
omr <coding.web.oomura@gmail.com>
2023-11-02 14:05:58 +0900

    css修正

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,34 +1,82 @@
-$(function(){$(window).on("scroll",function(){let elem=$(".fadeIn");const isAnimate="isAnimate";elem.each(function(){const elemOffset=$(this).offset().top;const scrollPos=$(window).scrollTop();const wh=$(window).height();if(scrollPos>elemOffset-wh+wh/3){$(this).addClass(isAnimate)}})});const $trigger=$("#hamburger");$trigger.on("click",function(){if($("#hamburger").hasClass("open")){$("#hamburger").removeClass("open");$("#gnav").removeClass("open")}else{$("#hamburger").addClass("open");$("#gnav").addClass("open")}});document.getElementById("gnav").addEventListener("click",function(){$("#hamburger").removeClass("open");$("#gnav").removeClass("open")})
+$(function () {
+  $(window).on("scroll", function () {
+    let elem = $(".fadeIn");
+    const isAnimate = "isAnimate";
+    elem.each(function () {
+      const elemOffset = $(this).offset().top;
+      const scrollPos = $(window).scrollTop();
+      const wh = $(window).height();
+      if (scrollPos > elemOffset - wh + wh / 3) {
+        $(this).addClass(isAnimate);
+      }
+    });
+  });
+  $(window).on("scroll", function () {
+    let el = $(".features__item");
+    const isFadeup = "isFadeup";
+    el.each(function () {
+      const elOffset = $(this).offset().top;
+      const scrollPos = $(window).scrollTop();
+      const wh = $(window).height();
+      if (scrollPos > elOffset - wh + wh / 3) {
+        $(this).addClass(isFadeup);
+      }
+    });
+  });
+
+  const $trigger = $("#hamburger");
+  $trigger.on("click", function () {
+    if ($("#hamburger").hasClass("open")) {
+      $("#hamburger").removeClass("open");
+      $("#gnav").removeClass("open");
+    } else {
+      $("#hamburger").addClass("open");
+      $("#gnav").addClass("open");
+    }
+  });
+  document.getElementById("gnav").addEventListener("click", function () {
+    $("#hamburger").removeClass("open");
+    $("#gnav").removeClass("open");
+  });
   //簡易バリデーション
   //--------------------------------------------
-  $('input,textarea,select').each(function(){
-
-    $(this).on('change',function(){
-
-      if($(this).is(':invalid')) {
-        $(this).parents('.input-field').addClass('is-error');
+  $("input,textarea,select").each(function () {
+    $(this).on("change", function () {
+      if ($(this).is(":invalid")) {
+        $(this).parents(".input-field").addClass("is-error");
         //そのフォームのエラーメッセージをスクリーンリーダー向けに表示
-        $(this).parents('.input-field').find('.error-text').attr('aria-hidden',false);
-
-      }else {
-        $(this).parents('.input-field').removeClass('is-error');
+        $(this)
+          .parents(".input-field")
+          .find(".error-text")
+          .attr("aria-hidden", false);
+      } else {
+        $(this).parents(".input-field").removeClass("is-error");
         //エラーメッセージをスクリーンリーダーから隠す
-        $(this).parents('.input-field').find('.error-text').attr('aria-hidden',true);
+        $(this)
+          .parents(".input-field")
+          .find(".error-text")
+          .attr("aria-hidden", true);
       }
     });
   });
 
-  $('#submit').on('click',function(){
-    $('input,textarea,select').each(function(){
-      if($(this).is(':invalid')) {
-        $(this).parents('.input-field').addClass('is-error');
+  $("#submit").on("click", function () {
+    $("input,textarea,select").each(function () {
+      if ($(this).is(":invalid")) {
+        $(this).parents(".input-field").addClass("is-error");
         //そのフォームのエラーメッセージをスクリーンリーダー向けに表示
-        $(this).parents('.input-field').find('.error-text').attr('aria-hidden',false);
-      }else {
-        $(this).parents('.input-field').removeClass('is-error');
+        $(this)
+          .parents(".input-field")
+          .find(".error-text")
+          .attr("aria-hidden", false);
+      } else {
+        $(this).parents(".input-field").removeClass("is-error");
         //エラーメッセージをスクリーンリーダーから隠す
-        $(this).parents('.input-field').find('.error-text').attr('aria-hidden',true);
+        $(this)
+          .parents(".input-field")
+          .find(".error-text")
+          .attr("aria-hidden", true);
       }
     });
   });
-})
+});
